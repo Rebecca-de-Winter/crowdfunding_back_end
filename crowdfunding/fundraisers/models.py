7ffa9a1871ceef85e004986086dd0b16191524b4
Rebecca Cole <coco_1981@hotmail.com>
2025-11-18 14:43:20 +1000

    added new migrations and serializers

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -9,6 +10,11 @@ class Fundraiser(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owned_fundraisers'
+    )
 
 class Pledge(models.Model):
     TYPE_OF_PLEDGE = [
@@ -22,6 +28,11 @@ class Pledge(models.Model):
     anonymous = models.BooleanField()
     fundraiser = models.ForeignKey(
         'Fundraiser',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
         on_delete=models.CASCADE,
         related_name='pledges'
     )
