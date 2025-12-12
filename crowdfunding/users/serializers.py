b82d463c7eea0331308b68fa48e29dd0e221def8
Rebecca Cole <coco_1981@hotmail.com>
2025-12-12 13:18:17 +1000

    make email in users write only

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -5,7 +5,11 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {"write_only": True}
+            }
+        
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
